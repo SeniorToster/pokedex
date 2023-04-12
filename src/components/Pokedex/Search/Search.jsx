c7143c8f1f978c0368796f4edbce22654f960df0
SeniorToster <seraleks04@gmail.com>
2023-04-12 23:00:38 +0300

    создан поиск по имени покемона

--- src/components/Pokedex/Search/Search.jsx
+++ src/components/Pokedex/Search/Search.jsx
@@ -1,9 +1,23 @@
 import styles from './Search.module.scss';
 import icon from '../../../assets/pokeball-icon.svg';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchPokemonDetails } from '../../../store/pokemonsSlice';
 function Search() {
+  const [text, setText] = useState('');
+  const dispatch = useDispatch();
+
   return (
-    <form className={styles.form}>
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        dispatch(fetchPokemonDetails(text.toLowerCase()));
+      }}
+      className={styles.form}
+    >
       <input
+        onChange={e => setText(e.target.value)}
+        value={text}
         type='text'
         className={styles.form_search}
         placeholder='Search your Pokemon!'
--- src/store/pokemonsSlice.js
+++ src/store/pokemonsSlice.js
@@ -3,7 +3,6 @@ import {
   getPokemon,
   getPokemonEvolution,
   getPokemonSpecies,
-  getPokemons,
   limit,
   url,
 } from '../api';
